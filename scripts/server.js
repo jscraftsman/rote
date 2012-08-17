b093b334cfbf64d7e0dc3685ef5de5ee0e11f2fe
Gervel Giva <gervelgiva@gmail.com>
2012-08-18 02:40:27 +0800

    working redis server

--- scripts/server.js
+++ scripts/server.js
@@ -3,6 +3,32 @@ io  = require('socket.io').listen(app),
 fs  = require('fs');
 var path = require('path');
 
+////////////////////////////////
+
+ var redis = require("redis"),
+        client = redis.createClient();
+
+    // if you'd like to select database 3, instead of 0 (default), call
+    // client.select(3, function() { /* ... */ });
+
+    client.on("error", function (err) {
+        console.log("Error " + err);
+    });
+
+    client.set("string key", "string val", redis.print);
+    client.hset("hash key", "hashtest 1", "some value", redis.print);
+    client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+    client.hkeys("hash key", function (err, replies) {
+        console.log(replies.length + " replies:");
+        replies.forEach(function (reply, i) {
+            console.log("    " + i + ": " + reply);
+        });
+        client.quit();
+    });
+
+
+ /////////////
+
 app.listen(3000);
 
 var CONTENT_TYPE = "text/plain";
